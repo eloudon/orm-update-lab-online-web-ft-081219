b8d2a4f0df5ec8c83ed7bab9948c7928375d82b0
Eric Loudon <loudon.eric@gmail.com>
2019-09-24 11:55:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,26 +7,26 @@ class Student
   attr_accessor :name, :grade
    attr_reader :id
 
-   def initialize(id = nil, name, grade)
+  def initialize(id = nil, name, grade)
      @name = name
      @grade = grade
      @id = id
-   end
+ end
 
-   def self.create_table
+  def self.create_table
      sql = <<-SQL
        CREATE TABLE IF NOT EXISTS students (
        id INTEGER PRIMARY KEY, name, TEXT, grade INTEGER);
      SQL
      DB[:conn].execute(sql)
-   end
+  end
 
-   def self.drop_table
+  def self.drop_table
      sql = "DROP TABLE IF EXISTS students"
      DB[:conn].execute(sql)
-   end
+  end
 
-   def save
+ def save
      if self.id
        self.update
      else
@@ -36,27 +36,32 @@ class Student
        SQL
        DB[:conn].execute(sql, self.name, self.grade)
        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-     end
    end
+ end
 
-   def self.create(name, grade)
+ def self.create(name, grade)
      student = Student.new(name, grade)
      student.save
      student
-   end
+  end
 
-   def self.new_from_db(row)
+ def self.new_from_db(row)
      student = Student.new(row[0], row[1], row[2])
-   end
+  end
 
-   def self.find_by_name(name)
+  def self.find_by_name(name)
      sql = <<-SQL
        SELECT * FROM students WHERE name = ? LIMIT 1;
       SQL
 
      DB[:conn].execute(sql, name).map do |row|
        self.new_from_db(row)
-     end.first
-   end
+   end.first
+  end
 
+ def update
+  sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+  DB[:conn].execute(sql, self.name, self.grade, self.id)
+
+ end
 end
